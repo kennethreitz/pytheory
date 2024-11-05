3ca8f541c847f0fd9319701642a174b58a801bc7
Kenneth Reitz <me@kennethreitz.org>
2024-11-05 10:29:33 -0500

    Refactor imports to optimize module usage

--- pytheory/__init__.py
+++ pytheory/__init__.py
@@ -1,1 +1,8 @@
-from .core import *
+from math import ceil, floor
+
+from .tones import Tone
+from .systems import System, SYSTEMS
+from .scales import Scale, TonedScale
+from .chords import Chord, Fretboard
+from .charts import CHARTS, charts_for_fretboard
+from .play import play, Synth
--- pytheory/core.py
+++
@@ -1,8 +0,0 @@
-from math import ceil, floor
-
-from .tones import Tone
-from .systems import System, SYSTEMS
-from .scales import Scale, TonedScale
-from .chords import Chord, Fretboard
-from .charts import CHARTS, charts_for_fretboard
-from .play import play, Synth
